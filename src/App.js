2f4b67b05ec0256a7939fb05687bb378e70f6a1a
Rajesh Garg <gargrajesh009@gmail.com>
2023-03-02 09:10:07 +0530

    textutil app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,47 @@
+import { useState } from 'react';
 import './App.css';
+import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+// import TextForm from './components/TextForm';
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from 'react-router-dom'
 
 function App() {
+  const [mode, setMode] = useState('light');
+  const toggleMode  = () =>{
+    if(mode==='light'){
+      setMode('dark');
+      document.body.style.backgroundColor = '#0f1b26';
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor = 'grey';
+    }
+  };
+  const changeBGColor1 = () =>{
+    document.body.style.backgroundColor = 'red';
+  }
+  const changeBGColor2 = () =>{
+    document.body.style.backgroundColor = 'blue';
+  }
+  const changeBGColor3 = () =>{
+    document.body.style.backgroundColor = 'green';
+  }
   return (
     <>
-    <Navbar/>
-    <div className="container">
-    <TextForm  heading="Please Enter your text here"/>
+    <Navbar toggle={toggleMode} mode={mode} changebg1={changeBGColor1} changebg2={changeBGColor2} changebg3={changeBGColor3}/>
+    <div className="container" >
+    <Router>
+      <Routes>
+        <Route path="/about" element={<About />} />
+        <Route path="/" element={<TextForm  mode={mode} heading="Please Enter your text here"/>} />
+      </Routes>
+    </Router>
     </div>
     </>
   );
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -4,15 +4,19 @@ export default function TextForm(Props) {
     const [text, setText] = useState('');
     const handleOnChange = (event) =>{
         setText(event.target.value);
-    }
+    };
     const changeToUpCase = () =>{
         let newText = text.toUpperCase();
         setText(newText);
-    }
+    };
+    const changeToLoCase = () =>{
+        let newText = text.toLowerCase();
+        setText(newText);
+    };
   return (
     <>
         <div className="mb-3">
-            <h3>{Props.heading}</h3>
+            <h2 className={`text-${Props.mode==='light'?'dark':'light'}`}>{Props.heading}</h2>
           <textarea
             className="form-control"
             id="textArea"
@@ -21,9 +25,18 @@ export default function TextForm(Props) {
             onChange={handleOnChange}
           ></textarea>
         </div>
-        <button type="submit" onClick = {changeToUpCase} className="btn btn-primary">
+        <button onClick = {changeToUpCase} className="btn btn-primary mx-3">
           ConvertToUpperCase
         </button>
+        <button  onClick = {changeToLoCase} className="btn btn-primary">
+          ConvertToLowerCase
+        </button>
+        <div className="container my-3">
+            <h2 className={`text-${Props.mode==='light'?'dark':'light'}`}>Your Text Summary</h2>
+            <p className={`text-${Props.mode==='light'?'dark':'light'}`}>Words: {text.length?text.split(' ').length:text.split(' ').length-1} Characters: {text.length}</p>
+            <h2 className={`text-${Props.mode==='light'?'dark':'light'}`}>Preview</h2>
+            <p className="my-2">{text}</p>
+        </div>
     </>
   );
 }
